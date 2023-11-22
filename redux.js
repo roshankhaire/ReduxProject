ea080ac60407d6123b36c610eab96f7913bd134b
roshankhaire <roshankhaire1992@gmail.com>
2023-11-22 18:30:22 +0530

    reduxCounter

---
+++ redux.js
@@ -0,0 +1,28 @@
+
+const redux=require('redux')
+const counterReducer=(state={counter:0},action)=>{
+    if(action.type==='INCREMENTBY2'){
+        return{
+
+            counter:state.counter+2
+         }
+    }
+    if(action.type==='DECREMENTBY2'){
+        return{
+            counter:state.counter-2
+         }
+    }
+       return state 
+    }
+   
+   
+
+const store=redux.createStore(counterReducer)
+//console.log(store.getState())
+const counterSubscriber=()=>{
+    const latestState=store.getState()
+    console.log(latestState)
+}
+store.subscribe(counterSubscriber)
+store.dispatch({type:'INCREMENTBY2'})
+store.dispatch({type:'DECREMENTBY2'})
